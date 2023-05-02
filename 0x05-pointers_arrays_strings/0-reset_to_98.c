1f0ceb4cc7bb2cb53356b3311c1b7e8d3b0e2d1c
Mehce <eguzoromercy7@gmail.com>
2023-05-02 06:28:41 -0700

    98 battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * reset_to_98 - update the value of n to 98
- * @n: input
- * return: n
+ * @n: value to be updated.
+ * return: void
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 
