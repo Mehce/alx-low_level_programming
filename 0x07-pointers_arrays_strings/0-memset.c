a55aaa0b8c4aed5bdd75fdaa8035fbf837c711d4
Mehce <eguzoromercy7@gmail.com>
2023-05-08 19:03:38 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _memset - fill a block of memory with a specific value
  * @s: starting address of memory to be filled
@@ -18,20 +20,4 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 
 	return (s);
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char buffer[98] = {0x00};
-
-	simple_print_buffer(buffer, 98);
-	_memset(buffer, 0x01, 95);
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);    
-	return (0);
 }
